9d0a2d2a756b443d20ee9eef4cf2d4e306b08d2d
Ishmael <151128995+Ishmael-quant@users.noreply.github.com>
2025-12-30 14:02:02 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  cout <<"Hi there! Welcome to my first cpp github project" << '\n';
+  cout <<"Type your name here to let me know who visited :";
+  string name;
+  cin >> name;
+  cout << "Thanks,"<< name;
+  return 0;// Used to know if there is an error
+}
