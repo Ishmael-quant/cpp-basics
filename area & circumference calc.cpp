71d5243470bf473597ca61cf9ab04e3a3ca4b723
Ishmael <151128995+Ishmael-quant@users.noreply.github.com>
2026-01-01 11:35:09 +0300

    Create area & circumference calc.cpp

---
+++ area & circumference calc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namepsace std;
+
+int main () {
+  cout << "Hi there! Welcome to circle area and calculator.";'\n';
+  cout <<"We are going to start with the circumference then the area.";'\n';
+  const double pi = 3.142;
+  double raduis;
+  cout << "Enter radius:";
+  cin >> radius;
+  double ans1 = 2 * pi * radius;
+  cout << "Here is the answer :" << ans1 ; << '\n';
+  cout >> "Lets continue to area. Input the radius"; << '\n';
+  cin >> rad;// radius for the area
+  double ans2 = pi * rad * rad;
+  cout << "Area is :"; << ans2;
+  
+  return 0;
+}
